eafb667daee1c3aa9e04c5e4364d7117a5a3fc26
Steyn van Litsenborgh <steyn.van.l@gmail.com>
2024-08-26 16:06:25 +0200

    django: Add `books` example Django app.

---
+++ django/books/accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+custom_user = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = custom_user
+    list_display = ['email', 'username']
+
+
+admin.site.register(custom_user, CustomUserAdmin)
---
+++ django/books/accounts/tests.py
@@ -0,0 +1,59 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+
+class CustomUserTest(TestCase):
+
+    def test_create_user(self):
+        user = get_user_model()
+        user = user.objects.create_user(
+            username='Will',
+            email='will@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(user.username, 'Will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        user = get_user_model()
+        admin_user = user.objects.create_superuser(
+            username='superadmin',
+            email='superadmin@email.com',
+            password='testpass123'
+        )
+
+        self.assertEqual(admin_user.username, 'superadmin')
+        self.assertEqual(admin_user.email, 'superadmin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+
+
+class SignupTests(TestCase):
+
+    username = 'newuser'
+    email = 'newuser@email.com'
+
+    def setUp(self):
+        url = reverse('account_signup')
+        self.response = self.client.get(url)
+
+    def test_signup_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'account/signup.html')
+        self.assertContains(self.response, 'Sign Up')
+        self.assertNotContains(self.response, 'Hi there')
+
+    def test_signup_form(self):
+        form = self.response.context.get('form')
+        new_user = get_user_model().objects.create_user(
+            self.username, self.email
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
